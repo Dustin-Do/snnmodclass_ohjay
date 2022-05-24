2944750ad7f6ccdeb35750d0bb841a95c333ca87
Huu Duc DO <58935702+Dustin-Do@users.noreply.github.com>
2022-05-24 10:35:45 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# SNN Modulation Classification
+
+Training (quantized) spiking neural networks with Deep Continuous Local Learning to classify modulations of radio signals.
+
+### Usage
+```
+pip install -r requirements.txt
+./scripts/train_radio_ml.sh
+```
---
+++ scripts/train_mnist.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+data="MNIST"
+network_spec="networks/mnist_conv.yaml"
+ref_network_spec="networks/mnist_conv.yaml"
+
+burnin=50
+n_iters=500
+n_iters_test=1500
+batch_size=128
+batch_size_test=512
+n_test_samples=1024
+
+python train.py \
+    --data $data \
+    --network_spec $network_spec \
+    --ref_network_spec $ref_network_spec \
+    --burnin $burnin \
+    --n_iters $n_iters \
+    --n_iters_test $n_iters_test \
+    --batch_size $batch_size \
+    --batch_size_test $batch_size_test \
+    --n_test_samples $n_test_samples
